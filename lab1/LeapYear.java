0b89f69715b0eda1306b4d4d3873d321f5a28ee7
“Qi” <460349853@qq.com>
2024-07-05 20:38:09 +0800

    completed LeapYear

--- lab1/LeapYear.java
+++ lab1/LeapYear.java
@@ -1,5 +1,5 @@
 /** Class that determines whether or not a year is a leap year.
- *  @author YOUR NAME HERE
+ *  @Qi Jiang
  */
 public class LeapYear {
 
@@ -27,6 +27,15 @@ public class LeapYear {
             } catch (NumberFormatException e) {
                 System.out.printf("%s is not a valid number.\n", args[i]);
             }
+        }
+    }
+    
+    /** Method that check whether or not a year is a leap year. */
+    public static boolean isLeapYear(int year) {
+        if ((year % 400 == 0) || (year % 4 == 0 && year % 100 != 0)){
+            return true;
+        } else {
+            return false;
         }
     }
 }
